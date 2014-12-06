c16aa91d27e55df28b35a365917fdae49108fb5d
Aina <ainasoga@gmail.com>
2014-12-06 14:24:13 +0100

    He intentat arreglar lo del sound... per algun motiu misteriós no chuta

--- roach.lua
+++ roach.lua
@@ -11,24 +11,34 @@ function roach.loadAssets()
 	roach.sprites[3] = love.graphics.newQuad(roach.width, roach.height, roach.width, roach.height, roach.img:getDimensions())
 	roach.sound = {}
 	roach.sound.move = "assets/roach-move.mp3"
+	TEsound.playLooping(roach.sound.move)
+	TEsound.stop(roach.sound.move)
 end
 
 function roach.init()
 	roach.x = 0
 	roach.y = groundHeight - roach.height
 	roach.state = "alive"
+	roach.soundIsPlaying = false;
 end
 
 function roach.update(dt)
 	if (not (keys["a"] and keys["w"])) then
 		if (keys["a"]) then 
-			roach.x = roach.x - 5 
-			TEsound.play(roach.sound.move, "moveSound")
+			roach.x = roach.x - 5
+			if (not roach.soundIsPlaying) then
+				TEsound.resume(roach.sound.move)
+				roach.soundIsPlaying = true
+			end
 		elseif (keys["d"]) then
 			roach.x = roach.x + 5
-			TEsound.play(roach.sound.move, "moveSound")
+			if (not roach.soundIsPlaying) then
+				TEsound.resume(roach.sound.move)
+				roach.soundIsPlaying = true
+			end
 		else
-			TEsound.stop("movesound", false)
+			TEsound.pause(roach.sound.move)
+			roach.soundIsPlaying = false
 		end
 	end
 end
